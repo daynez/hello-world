b3d392d72d0de01a06658f2679d2fc7b5ecf81be
Dayne Zimmerman <daynez@zato.local>
2017-05-01 09:26:46 -0500

    Changed the input bugger from type int to string and used getline() as the input function rather than taking characters from the stdin buffer with cin.

--- flash.cpp
+++ flash.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
+#include <cstring>
 #include <ctime>
 
 #include "flash.h"
@@ -9,29 +10,32 @@ using namespace std;
 
 int main() {
 
-  int enters = 'y';
+  string enters = "y";
   int srand(time(NULL));
 
-  while(enters == 'y') {
+  cout << enters << endl;
+
+  while(!enters.compare("y")) {
     int rnum1 = rand()%12+1;
     int rnum2 = rand()%12+1;
-
+    int t;
     int bingo;
 
     bingo = rnum1 * rnum2;
 
-    cout << rnum1 << " times " << rnum2 << " equals?";
+    printf("%d times %d equals? ", rnum1, rnum2);
 
-    while(enters != 'q') {
-      cin >> enters;
-      if(enters == bingo) {
+    while(enters.compare("q")) {
+      getline(cin, enters);
+      if(bingo == (t = atoi(enters.c_str()))) {
         cout << "Congratulations! ";
         enters = 'q';
       }  else {
-        cout << "Sorry, try again ";
+        printf("Sorry, try again ");
       }
     }
-  cout << "Try another?";
-  cin >> enters;
+  printf("Try another?  ");
+  getline(cin, enters);
+  cout << enters << endl;
   }
 }
